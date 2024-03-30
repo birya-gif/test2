c492d3b7f38102144d1e31950d78e7d75baa3a86
birya <82666619+birya-gif@users.noreply.github.com>
2024-03-30 15:28:58 +0300

    Initial commit

---
+++ mainsql.py
@@ -0,0 +1,49 @@
+import pymysql
+from sql import host,user,password,dbname
+
+
+try:
+    conn = pymysql.connect(host=host,user=user,port=3306, passwd=password,database=dbname)
+    print('successfully connected to database')
+
+    while True:
+        try:
+
+            imp = input('Введите номер заказа:')
+            if imp == '':
+                break
+            imp=int(imp)
+            with conn.cursor() as cursor:
+                cursor.execute('SELECT id FROM shops.order')
+                id_orders = cursor.fetchall()
+                found = False
+
+                for id_order in id_orders:
+                    if id_order[0] == imp:
+                        found = True
+                        break
+
+                if found != True:
+                    print('Номер заказа введен не верно, попробуйте снова или нажмите enter чтобы выйти')
+
+                if found == True:
+                    cursor.execute(
+                        'SELECT shops.order.id, count, device.name, rack.stellage, rack_dop.stil_dop FROM shops.order INNER JOIN shops.main ON product_id = articul INNER JOIN shops.device ON device_id=device.id INNER JOIN shops.rack ON rack_main_id=rack.id INNER JOIN shops.rack_dop ON rack_dop_id=rack_dop.id '
+                        'WHERE shops.order.id = %s', (imp,))
+
+                    shoping = cursor.fetchall()
+                    for shop in shoping:
+
+                        print(f'Номер заказа:{imp},Стеллаж{shop[3]}, Устройство:{shop[2]}, Количество:{shop[1]},  Доп.стеллаж{shop[4]}')
+
+
+
+        except Exception as ex:
+            print('not successful')
+
+
+finally:
+    conn.close()
+
+
+
